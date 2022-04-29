d2548415bec6495be10c18f9ae8bda3fdf835983
JIHYUN <2ijhyun@gmail.com>
2022-04-30 01:52:51 +0900

    Add : Add QT App tutorial

---
+++ 02_Gui_App/widget.h
@@ -0,0 +1,25 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+#include <QWidget>
+#include <QPushButton>
+
+#include <QWidget>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+private:
+    QPushButton *btn;
+    QString str;
+
+public slots:
+    void slot_btn();
+
+};
+
+#endif // WIDGET_H
