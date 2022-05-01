0eb026842e47a1958d3958e24e6538fece4456da
JIHYUN <2ijhyun@gmail.com>
2022-05-01 16:16:33 +0900

    Add : Add QT app tutorial - QScrollArea

---
+++ 03_01_BasicWidget_16_QScrollArea/widget.cpp
@@ -0,0 +1,23 @@
+#include "widget.h"
+#include <QLabel>
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    QImage image;
+    QScrollArea *area;
+
+    QLabel *lbl = new QLabel(this);
+    image = QImage(":/resources/fish.png");
+    lbl->setPixmap(QPixmap::fromImage(image));
+
+    area = new QScrollArea(this);
+    area->setWidget(lbl);
+    area->setBackgroundRole(QPalette::Dark);
+    area->setGeometry(0, 0, image.width(), image.height());
+}
+
+Widget::~Widget()
+{
+
+}
