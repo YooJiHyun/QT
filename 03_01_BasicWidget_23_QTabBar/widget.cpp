8e1e09fb21589d130fee3930de5e9b43d9299037
JIHYUN <2ijhyun@gmail.com>
2022-05-06 23:08:22 +0900

    Add : Add QT app tutorial - QTabBar

---
+++ 03_01_BasicWidget_23_QTabBar/widget.cpp
@@ -0,0 +1,25 @@
+#include "widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+{
+    QTabBar *tab = new QTabBar(this);
+    tab->addTab("Browser");
+    tab->addTab("User");
+    tab->addTab("Application");
+    tab->setShape(QTabBar::RoundedNorth);
+    tab->setGeometry(20, 20, 300, 250);
+    tab->setTabTextColor(1, QColor(255,0,0));
+    connect(tab,SIGNAL(currentChanged(int)),this,SLOT(currentTab(int)));
+
+}
+
+Widget::~Widget()
+{
+
+}
+
+void Widget::currentTab(int index)
+{
+    qDebug("Current Tab : %d", index);
+}
---
+++ 03_01_BasicWidget_23_QTabBar/widget.h
@@ -0,0 +1,18 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QTabBar>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = 0);
+    ~Widget();
+
+private slots:
+    void currentTab(int index);
+};
+
+#endif // WIDGET_H
