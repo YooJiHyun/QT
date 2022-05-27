728e8920d476e2a3c4250b341a3b6c692c336965
JIHYUN <2ijhyun@gmail.com>
2022-05-28 00:45:12 +0900

    Add : Add QT app tutorial - MDI

---
+++ 03_07_MDI/mdimainwindow.h
@@ -0,0 +1,13 @@
+#ifndef MDIMAINWINDOW_H
+#define MDIMAINWINDOW_H
+
+#include <QMainWindow>
+#include <QObject>
+
+class MDIMainWindow : public QMainWindow
+{
+public:
+    explicit MDIMainWindow(QWidget *parent = nullptr);
+};
+
+#endif // MDIMAINWINDOW_H
